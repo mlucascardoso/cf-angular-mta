392cc82e4687e743c0db755c8a69f3079fb2ed87
Lucas Medeiros Cardoso <clucasmedeiros@gmail.com>
2021-12-20 13:41:00 -0300

    feat: add cap sample request

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import axios from 'axios';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+    title = 'app';
+
+    public async ngOnInit(): Promise<void> {
+        const data = {
+            supplierId: '1',
+            email: 'lucas.cardoso@vaees.com'
+        };
+        await axios.post('/api/supplier-management/Emails', data);
+        const result = await axios.get('/api/supplier-management/Emails');
+        console.log(result.data);
+    }
 }
